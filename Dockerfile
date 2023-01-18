d7155376fc7667ab0db2f52183bcf0cbae0cdf53
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2023-01-17 19:01:38 -0500

    composerize

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM rclone/rclone:1
-
-ENTRYPOINT []
-
-# This runs cron in the foreground with loglevel 2
-CMD [ "crond", "-l", "2", "-f" ]
--- rcloneSync.sh
+++ rcloneSync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-rclone --config="/etc/rclone.conf" sync /home/pi/server-setup/bind-mounts/nextcloud/data/blarvis/files OneBlarvis:ServerBackup/nextcloud-files/files --log-file /home/pi/rclone-logs.txt
+rclone --config="/etc/rclone.conf" sync /home/pi/server-setup/bind-mounts/nextcloud/data/blarvis/files OneBlarvis:ServerBackup/nextcloud-files/files --log-file /rclone-logs/rclone-logs.txt
