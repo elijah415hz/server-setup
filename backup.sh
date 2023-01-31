7fdc19e30b259f2e0e4fbcc5849092bdbff5a0d7
elijah415hz <elijahblaisdell@gmail.com>
2023-01-31 11:31:25 -0500

    some more fizes

--- backup.sh
+++ backup.sh
@@ -10,19 +10,21 @@ sendMail () {
         echo "Hooray" > /tmp/hooray
     fi
 
-    echo "Subject: $sub\nFrom: $GMAIL_ADDRESS\nTo: $GMAIL_ADDRESS\n\nError:\n\n$(cat $fileName)" > $fileName
+    body=$(cat $fileName)
 
     curl -s --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
     --mail-from $GMAIL_ADDRESS \
-    --mail-rcpt $GMAIL_ADDRESS\
+    --mail-rcpt $GMAIL_ADDRESS \
     --user $GMAIL_ADDRESS:$GMAIL_APP_PASSWORD \
-    -T $fileName
+    -H "Subject: $sub" \
+    -F "=$body;type=text/plain"
 }
 
 mkdir -p nextcloud-db-backup
 
 mysqldump --single-transaction -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE &> /tmp/sqlDumpOrErr.sql
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]
+then
     echo "script 1 returned non-zero code"
     sendMail "MysqlDump failed" /tmp/sqlDumpOrErr.sql
 else
@@ -30,16 +32,18 @@ else
     sendMail "MysqlDump succeeded"
 fi
 
-rclone --config="/config/rclone/rclone.conf" copy ./ServerBackup OneBlarvis:ServerBackup  --include /*.env &> /tmp/rclone-env-logs.txt
-if [ $? -ne 0 ]; then
+rclone --config="/config/rclone/rclone.conf" copy usr/ServerBackup OneBlarvis:ServerBackup  --include /*.env &> /tmp/rclone-env-logs.txt
+if [ $? -ne 0 ]
+then
     echo "script 2 returned non-zero code"
     sendMail "Rclone sync of env variables failed" /tmp/rclone-env-logs.txt
 else
     sendMail "Rclone sync of env variables succeeded"
 fi
 
-rclone --config="/config/rclone/rclone.conf" sync ./ServerBackup/bind-mounts OneBlarvis:ServerBackup  --exclude /caddy/** --exclude /nextcloud_db/** &> /tmp/rclone-bind-mounts-logs.txt
-if [ $? -ne 0 ]; then
+rclone --config="/config/rclone/rclone.conf" sync /usr/ServerBackup/bind-mounts OneBlarvis:ServerBackup/bind-mounts  --exclude /caddy/** --exclude /nextcloud_db/** &> /tmp/rclone-bind-mounts-logs.txt
+if [ $? -ne 0 ]
+then
     echo "script 3 returned non-zero code"
     sendMail "Rclone sync of bind-mounts failed" /tmp/rclone-bind-mounts-logs.txt
 else
