3bf89d4b6bdff1ec9feb7f75e050cd229650beeb
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2023-04-15 21:12:28 -0400

    add simple logging for devops debug

--- devops-listen-to-pipe.sh
+++ devops-listen-to-pipe.sh
@@ -1,6 +1,8 @@
 while true; do
     pipe_value=$(cat ./bind-mounts/devops/pipe)
 
+    echo $pipe_value >> ./devops-log.txt
+
     case "$pipe_value" in
         "deploy-finances-app") make deploy-finances;;
     esac
