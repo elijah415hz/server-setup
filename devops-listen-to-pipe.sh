1ffddec99d1dc3719a0922887500dfedc35d6c63
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2023-04-19 22:00:14 -0400

    Add absolute path to pipe listener

--- devops-listen-to-pipe.sh
+++ devops-listen-to-pipe.sh
@@ -1,7 +1,7 @@
 while true; do
-    pipe_value=$(cat ./bind-mounts/devops/pipe)
+    pipe_value=$(cat ~/server-setup/bind-mounts/devops/pipe)
 
-    echo $pipe_value >> ./devops-log.txt
+    echo $pipe_value >> ~/server-setup/bind-mounts/devops/devops-log.txt
 
     case "$pipe_value" in
         "deploy-finances-app") make deploy-finances;;
