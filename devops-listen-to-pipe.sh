30de9a9e01709c37eaad3912ccb0aaf53442910b
elijah415hz <elijahblaisdell@gmail.com>
2023-10-01 21:22:49 -0400

    Add fix and some logging to listen-to-pipe

--- devops-listen-to-pipe.sh
+++ devops-listen-to-pipe.sh
@@ -1,9 +1,11 @@
 while true; do
+    echo "listening to the pipe...."
     pipe_value=$(cat ~/server-setup/bind-mounts/devops/pipe)
 
     echo $pipe_value >> ~/server-setup/bind-mounts/devops/devops-log.txt
 
     case "$pipe_value" in
-        "deploy-finances-app") make deploy-finances;;
+        "deploy-finances-app") cd ~/server-setup && make deploy-finances && echo "deploy complete";;
     esac
+    echo "out of the case...."
 done
