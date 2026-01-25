5f09ea5ee73d7cc72272717110ba5b2865e04389
Elijah Blaisdell <elijahblaisdell@gmail.com>
2026-01-25 06:56:53 +0000

    update devops logging

--- devops-listen-to-pipe.sh
+++ devops-listen-to-pipe.sh
@@ -5,8 +5,8 @@ while true; do
     echo $pipe_value >> ~/server-setup/bind-mounts/devops/devops-log.txt
 
     case "$pipe_value" in
-        "deploy-finances-app") cd ~/server-setup && make deploy-finances 2>> ~/server-setup/bind-mounts/devops/devops-log.txt && echo "deploy complete" >> ~/server-setup/bind-mounts/devops/devops-log.txt;;
-        "deploy-finances-cron") cd ~/server-setup && make deploy-finances-cron 2>> ~/server-setup/bind-mounts/devops/devops-log.txt && echo "deploy complete" >> ~/server-setup/bind-mounts/devops/devops-log.txt;;
+        "deploy-finances-app") cd ~/server-setup && make deploy-finances 2>> ~/server-setup/bind-mounts/devops/devops-log.txt && echo "deploy complete $(date)" >> ~/server-setup/bind-mounts/devops/devops-log.txt;;
+        "deploy-finances-cron") cd ~/server-setup && make deploy-finances-cron 2>> ~/server-setup/bind-mounts/devops/devops-log.txt && echo "deploy complete $(date)" >> ~/server-setup/bind-mounts/devops/devops-log.txt;;
     esac
     echo "out of the case...."
 done
